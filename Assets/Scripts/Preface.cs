8695bbaee65540a78af4504528bfc5d0bb7ac86a
Mao-Hao <mao_hao@outlook.com>
2021-10-12 19:24:29 -0400

    分数和开场白

---
+++ Assets/Scripts/Preface.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Preface : MonoBehaviour {
+    GameObject prefaceGO;
+    public RawImage rawImg;
+
+
+    void Awake()
+    {
+        prefaceGO = GameObject.Find( "Preface" );
+        rawImg = prefaceGO.GetComponent<RawImage>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating( "Fade", 0, 0.002f );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Fade()
+    {
+        float delta = 0.003f;
+
+        rawImg.color = Color.Lerp( rawImg.color, Color.clear, delta * (1.001f - rawImg.color.a) );
+
+        if ( rawImg.color.a < 0.1f ) {
+            prefaceGO.SetActive( false );
+            Destroy( prefaceGO );
+        }
+        
+    }
+}
